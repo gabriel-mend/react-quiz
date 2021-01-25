9a2c5c39a349aeb578ce1d19f629eef8e6fc12ed
Gabriel <gabrielmendoncapereira14@gmail.com>
2021-01-25 04:22:21 -0300

    First day of immersion

---
+++ src/pages/index.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+
+import db from '../../db.json'
+import Widget from '../components/Widget'
+import QuizLogo from '../components/QuizLogo'
+import QuizBackground from '../components/QuizBackground'
+import Footer from '../components/Footer'
+import GitHubCorner from '../components/GitHubCorner'
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+export default function Home() {
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+    </QuizBackground>
+  );
+}
